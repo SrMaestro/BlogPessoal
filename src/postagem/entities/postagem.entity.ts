47a543c4939ea102808681cd149bc4b5cccac945
Luis Felipe Ramires Leão <luisramires29032002@gmail.com>
2025-12-10 17:48:24 -0300

    feat: - Criar a Classe Entidade Usuario - Criar a Relação ManytoOne na Classe Postagem - Registrar a Classe Usuario na Classe UsuarioModule

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -1,6 +1,7 @@
 import { IsNotEmpty } from "class-validator";
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { Tema } from "../../tema/entities/tema.entity";
+import { Usuario } from "../../usuario/entities/usuario.entity";
 
 
 @Entity({ name: "tb_postagens" })
@@ -24,6 +25,11 @@ export class Postagem {
         onDelete: "CASCADE"
     })
     tema: Tema
+
+    @ManyToOne(() => Usuario, (usuario) => usuario.postagem, {
+        onDelete: "CASCADE"
+    })
+    usuario: Usuario
 
 
 
